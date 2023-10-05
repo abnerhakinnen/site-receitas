5277ff1a3b74191d305a132f75bc35155f65b197
abnerhakinnen <abnerhakinnen@alu.ufc.br>
2023-10-04 21:07:37 -0300

    commit teste

--- src/componentes/Feed/Feed.jsx
+++ src/componentes/Feed/Feed.jsx
@@ -40,7 +40,7 @@ const Feed = () => {
     <div className="max-w-[1200px] mx-auto">
       <div className="p-10 pb-0 text-orange-500">
         <h1 className="text-4xl font-bold text-center ml-4 md:text-left">
-          Receitas Aleatórias
+          TESTANDO
         </h1>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5 px-8">
